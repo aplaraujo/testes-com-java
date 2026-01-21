3943aa72df1d20f647425e9f3db58aad950a8760
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-01-21 11:41:24 -0300

    Parameterized testing @MethodSource()

---
+++ 04_ConceitosAvancadosDeJUnit5/automated-tests-with-java/src/main/java/io/github/aplaraujo/math/SimpleMath04.java
@@ -0,0 +1,30 @@
+package io.github.aplaraujo.math;
+
+public class SimpleMath04 {
+    public Double sum(Double firstNumber, Double secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    public Double subtract(Double firstNumber, Double secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    public Double multiply(Double firstNumber, Double secondNumber) {
+        return firstNumber * secondNumber;
+    }
+
+    public Double divide(Double firstNumber, Double secondNumber) {
+        if (secondNumber.equals(0D)) {
+            throw new ArithmeticException("Impossible to divide by zero!");
+        }
+        return firstNumber / secondNumber;
+    }
+
+    public Double mean(Double firstNumber, Double secondNumber) {
+        return (firstNumber + secondNumber) / 2;
+    }
+
+    public Double squareRoot(Double number) {
+        return Math.sqrt(number);
+    }
+}
