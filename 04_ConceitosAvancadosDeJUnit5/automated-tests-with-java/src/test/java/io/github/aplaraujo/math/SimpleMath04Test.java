2e8a7e48602d8e40e8d04573609896b086d2ea7c
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-01-23 18:57:03 -0300

    Parameterized test with @CsvSource

--- 04_ConceitosAvancadosDeJUnit5/automated-tests-with-java/src/test/java/io/github/aplaraujo/math/SimpleMath04Test.java
+++ 04_ConceitosAvancadosDeJUnit5/automated-tests-with-java/src/test/java/io/github/aplaraujo/math/SimpleMath04Test.java
@@ -1,13 +1,9 @@
 package io.github.aplaraujo.math;
 
-import java.util.stream.Stream;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class SimpleMath04Test {
     SimpleMath04 math;
@@ -17,20 +13,31 @@ class SimpleMath04Test {
         math = new SimpleMath04();
     }
 
-    @DisplayName("Test 10.0 / 2.0 = 5.0")
+    // @DisplayName("Test 10.0 / 2.0 = 5.0")
+    // @ParameterizedTest
+    // @MethodSource()
+    // void testDivide(double firstNumber, double secondNumber, double expected) {
+    
+    //     Double actual = math.divide(firstNumber, secondNumber);
+    //     assertEquals(expected, actual, () -> firstNumber + " / " + secondNumber + " did not produce " + expected + "!");
+    // }
+
+    // public static Stream<Arguments> testDivide() {
+    //     return Stream.of(
+    //         Arguments.of(6D, 2D, 3D),
+    //         Arguments.of(10D, 2D, 5D)
+    //     );
+    // }
+
     @ParameterizedTest
-    @MethodSource()
+    @CsvSource({
+        "6.0, 2.0, 3.0",
+        "10.0, 2.0, 5.0"
+    })
     void testDivide(double firstNumber, double secondNumber, double expected) {
     
         Double actual = math.divide(firstNumber, secondNumber);
         assertEquals(expected, actual, () -> firstNumber + " / " + secondNumber + " did not produce " + expected + "!");
-    }
-
-    public static Stream<Arguments> testDivide() {
-        return Stream.of(
-            Arguments.of(6D, 2D, 3D),
-            Arguments.of(10D, 2D, 5D)
-        );
     }
 }
     
