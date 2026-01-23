b3da0437c1e6b8b60c99ef243c990eae33bacbaa
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-01-23 18:59:38 -0300

    Parameterized test with @CsvFileSource

--- 04_ConceitosAvancadosDeJUnit5/automated-tests-with-java/src/test/java/io/github/aplaraujo/math/SimpleMath04Test.java
+++ 04_ConceitosAvancadosDeJUnit5/automated-tests-with-java/src/test/java/io/github/aplaraujo/math/SimpleMath04Test.java
@@ -3,7 +3,7 @@ package io.github.aplaraujo.math;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 class SimpleMath04Test {
     SimpleMath04 math;
@@ -30,10 +30,11 @@ class SimpleMath04Test {
     // }
 
     @ParameterizedTest
-    @CsvSource({
-        "6.0, 2.0, 3.0",
-        "10.0, 2.0, 5.0"
-    })
+    // @CsvSource({
+    //     "6.0, 2.0, 3.0",
+    //     "10.0, 2.0, 5.0"
+    // })
+    @CsvFileSource(resources="/test.csv")
     void testDivide(double firstNumber, double secondNumber, double expected) {
     
         Double actual = math.divide(firstNumber, secondNumber);
