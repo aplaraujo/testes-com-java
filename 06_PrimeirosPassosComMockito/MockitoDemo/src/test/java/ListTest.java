5c36323f53f2b13ff7e1394272187a84db0b3e4f
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-01-29 09:59:31 -0300

    Mocking lists

---
+++ 06_PrimeirosPassosComMockito/MockitoDemo/src/test/java/ListTest.java
@@ -0,0 +1,53 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ListTest {
+    // Teste para simular o tamanho de uma lista
+    @Test
+    void testMockingListWhenSizeIsCalledShouldReturn10() {
+        // Arrange
+        List<?> list = mock(List.class);
+        when(list.size()).thenReturn(10);
+        // Act
+        // Assert
+        assertEquals(10, list.size());
+    }
+
+    @Test
+    void testMockingListWhenSizeIsCalledShouldReturnMultipleValues() {
+        // Arrange
+        List<?> list = mock(List.class);
+        when(list.size()).thenReturn(10).thenReturn(20);
+        // Act
+        // Assert
+        assertEquals(10, list.size());
+        assertEquals(20, list.size());
+        assertEquals(20, list.size());
+    }
+
+    @Test
+    void testMockingListWhenGetIsCalledShouldReturnAninha() {
+        // Arrange
+        var list = mock(List.class);
+        when(list.getFirst()).thenReturn("Aninha");
+        // Act
+        // Assert
+        assertEquals("Aninha", list.getFirst());
+        assertNull(list.get(1));
+    }
+
+    @Test
+    void testMockingListWhenGetIsCalledWithArgumentMatcherShouldReturnAninha() {
+        // Arrange
+        var list = mock(List.class);
+        when(list.get(anyInt())).thenReturn("Aninha");
+        // Act
+        // Assert
+        assertEquals("Aninha", list.get(anyInt()));
+    }
+
+}
