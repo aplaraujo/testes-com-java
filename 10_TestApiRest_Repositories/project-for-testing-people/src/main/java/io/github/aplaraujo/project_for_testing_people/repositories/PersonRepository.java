b06209d34fb57948a15e323e217907c844b731d0
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-01-30 17:19:50 -0300

    Person repository

---
+++ 10_TestApiRest_Repositories/project-for-testing-people/src/main/java/io/github/aplaraujo/project_for_testing_people/repositories/PersonRepository.java
@@ -0,0 +1,25 @@
+package io.github.aplaraujo.project_for_testing_people.repositories;
+
+import io.github.aplaraujo.project_for_testing_people.entities.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+    Optional<Person> findByEmail(String email);
+
+    @Query("select p from Person p where p.firstName =?1 and p.lastName = ?1")
+    Person findByJPQL(String firstName, String lastName);
+
+    @Query("select p from Person p where p.firstName = :firstName and p.lastName = :lastName")
+    Person findByJPQLNamedParameters(@Param("FirstName") String firstName, @Param("lastName") String lastName);
+
+    @Query(value = "SELECT * FROM person p WHERE p.first_name = ?1 AND p.last_name = ?1", nativeQuery = true)
+    Person findByNativeSQL(String firstName, String lastName);
+
+    @Query(value = "SELECT * FROM person p WHERE p.first_name = :firstName AND p.last_name = :lastName", nativeQuery = true)
+    Person findByNativeSQLWithNamedParameters(@Param("FirstName") String firstName, @Param("lastName") String lastName);
+
+}
