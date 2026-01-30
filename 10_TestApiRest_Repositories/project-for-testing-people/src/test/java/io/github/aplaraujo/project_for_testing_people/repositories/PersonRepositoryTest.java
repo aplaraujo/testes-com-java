7e0e0eaa0ab20221a3eaa69955fa84df76b2ab54
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-01-30 20:07:52 -0300

    Find all people repository testing

--- 10_TestApiRest_Repositories/project-for-testing-people/src/test/java/io/github/aplaraujo/project_for_testing_people/repositories/PersonRepositoryTest.java
+++ 10_TestApiRest_Repositories/project-for-testing-people/src/test/java/io/github/aplaraujo/project_for_testing_people/repositories/PersonRepositoryTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
@@ -19,5 +21,18 @@ class PersonRepositoryTest {
         Person savedPerson = repository.save(person);
         assertNotNull(savedPerson);
         assertTrue(savedPerson.getId() > 0);
+    }
+
+    @Test
+    public void testGivenPersonObject_whenFindAll_thenShouldReturnPersonList() {
+        Person person = new Person("Betina", "Farias", "Avenida André Antônio Maggi, 636", "Feminino", "betina.isabella.farias@tecnew.net");
+        Person person1 = new Person("Tereza", "Assis", "Alameda Venezuela, 937", "Feminino", "terezacarolinaassis@imaxbrasil.com.br");
+        repository.save(person);
+        repository.save(person1);
+
+        List<Person> personList = repository.findAll();
+
+        assertNotNull(person);
+        assertEquals(20, personList.size());
     }
 }
