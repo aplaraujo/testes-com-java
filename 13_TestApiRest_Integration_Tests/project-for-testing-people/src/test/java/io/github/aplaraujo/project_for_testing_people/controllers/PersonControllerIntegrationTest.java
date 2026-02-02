65002936ef495d3d171cdf40d3d3681982f9308f
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-02-02 15:04:31 -0300

    Person creation and update integration tests

--- 13_TestApiRest_Integration_Tests/project-for-testing-people/src/test/java/io/github/aplaraujo/project_for_testing_people/controllers/PersonControllerIntegrationTest.java
+++ 13_TestApiRest_Integration_Tests/project-for-testing-people/src/test/java/io/github/aplaraujo/project_for_testing_people/controllers/PersonControllerIntegrationTest.java
@@ -1,5 +1,6 @@
 package io.github.aplaraujo.project_for_testing_people.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.aplaraujo.project_for_testing_people.config.TestConfig;
@@ -10,13 +11,16 @@ import io.restassured.filter.log.LogDetail;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.specification.RequestSpecification;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.*;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ActiveProfiles("dev")
 public class PersonControllerIntegrationTest extends AbstractIntegrationTest {
 
     // Definir como a requisição será feita
@@ -30,7 +34,7 @@ public class PersonControllerIntegrationTest extends AbstractIntegrationTest {
         objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
 
         specification = new RequestSpecBuilder()
-                .setBasePath("")
+                .setBasePath("/person")
                 .setPort(TestConfig.SERVER_PORT)
                 .addFilter(new RequestLoggingFilter(LogDetail.ALL))
                 .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
@@ -38,5 +42,75 @@ public class PersonControllerIntegrationTest extends AbstractIntegrationTest {
 
         person = new Person("Betina", "Farias", "Avenida André Antônio Maggi, 636", "Feminino", "betina.isabella.farias@tecnew.net");
         person1 = new Person("Tereza", "Assis", "Alameda Venezuela, 937", "Feminino", "terezacarolinaassis@imaxbrasil.com.br");
+    }
+
+
+    @Test
+    @Order(1)
+    public void integrationTestGivenPersonObject_WhenCreateAPerson_ShouldReturnPersonObject() throws JsonProcessingException {
+        var content = given()
+                .spec(specification)
+                .contentType(TestConfig.CONTENT_TYPE_JSON)
+                .body(person)
+                .when()
+                .post()
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .asString();
+        // Conversão do corpo da requisição de JSON para objeto
+        Person createdPerson = objectMapper.readValue(content, Person.class);
+        person = createdPerson;
+
+        assertNotNull(createdPerson);
+        assertNotNull(createdPerson.getId());
+        assertNotNull(createdPerson.getFirstName());
+        assertNotNull(createdPerson.getLastName());
+        assertNotNull(createdPerson.getAddress());
+        assertNotNull(createdPerson.getGender());
+        assertNotNull(createdPerson.getEmail());
+
+        assertTrue(createdPerson.getId() > 0);
+        assertEquals("Betina", createdPerson.getFirstName());
+        assertEquals("Farias", createdPerson.getLastName());
+        assertEquals("Avenida André Antônio Maggi, 636", createdPerson.getAddress());
+        assertEquals("Feminino", createdPerson.getGender());
+        assertEquals("betina.isabella.farias@tecnew.net", createdPerson.getEmail());
+    }
+
+    @Test
+    @Order(2)
+    public void integrationTestGivenPersonObject_WhenUpdateAPerson_ShouldReturnUpdatedPersonObject() throws JsonProcessingException {
+        person.setFirstName("Betina Isabella");
+        var content = given()
+                .spec(specification)
+                .contentType(TestConfig.CONTENT_TYPE_JSON)
+                .body(person)
+                .when()
+                .put()
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .asString();
+        // Conversão do corpo da requisição de JSON para objeto
+        Person createdPerson = objectMapper.readValue(content, Person.class);
+        person = createdPerson;
+
+        assertNotNull(createdPerson);
+        assertNotNull(createdPerson.getId());
+        assertNotNull(createdPerson.getFirstName());
+        assertNotNull(createdPerson.getLastName());
+        assertNotNull(createdPerson.getAddress());
+        assertNotNull(createdPerson.getGender());
+        assertNotNull(createdPerson.getEmail());
+
+        assertTrue(createdPerson.getId() > 0);
+        assertEquals("Betina Isabella", createdPerson.getFirstName());
+        assertEquals("Farias", createdPerson.getLastName());
+        assertEquals("Avenida André Antônio Maggi, 636", createdPerson.getAddress());
+        assertEquals("Feminino", createdPerson.getGender());
+        assertEquals("betina.isabella.farias@tecnew.net", createdPerson.getEmail());
     }
 }
