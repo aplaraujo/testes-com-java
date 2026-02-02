c78f9da1b0d763749db3ddd7a33180267e86a50b
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-02-02 15:26:59 -0300

    Person find person by id and find all people integration tests

--- 13_TestApiRest_Integration_Tests/project-for-testing-people/src/test/java/io/github/aplaraujo/project_for_testing_people/controllers/PersonControllerIntegrationTest.java
+++ 13_TestApiRest_Integration_Tests/project-for-testing-people/src/test/java/io/github/aplaraujo/project_for_testing_people/controllers/PersonControllerIntegrationTest.java
@@ -15,6 +15,8 @@ import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.List;
+
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -112,5 +114,95 @@ public class PersonControllerIntegrationTest extends AbstractIntegrationTest {
         assertEquals("Avenida André Antônio Maggi, 636", createdPerson.getAddress());
         assertEquals("Feminino", createdPerson.getGender());
         assertEquals("betina.isabella.farias@tecnew.net", createdPerson.getEmail());
+    }
+
+    @Test
+    @Order(3)
+    public void integrationTestGivenPersonObject_WhenFindAPersonById_ShouldReturnAPersonObject() throws JsonProcessingException {
+
+        var content = given()
+                .spec(specification)
+                .pathParam("id", person.getId())
+                .when()
+                .get("{id}")
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .asString();
+        // Conversão do corpo da requisição de JSON para objeto
+        Person createdPerson = objectMapper.readValue(content, Person.class);
+
+        assertNotNull(createdPerson);
+        assertNotNull(createdPerson.getId());
+        assertNotNull(createdPerson.getFirstName());
+        assertNotNull(createdPerson.getLastName());
+        assertNotNull(createdPerson.getAddress());
+        assertNotNull(createdPerson.getGender());
+        assertNotNull(createdPerson.getEmail());
+
+        assertTrue(createdPerson.getId() > 0);
+        assertEquals("Betina Isabella", createdPerson.getFirstName());
+        assertEquals("Farias", createdPerson.getLastName());
+        assertEquals("Avenida André Antônio Maggi, 636", createdPerson.getAddress());
+        assertEquals("Feminino", createdPerson.getGender());
+        assertEquals("betina.isabella.farias@tecnew.net", createdPerson.getEmail());
+    }
+
+    @Test
+    @Order(4)
+    public void integrationTest_WhenFindAllPeople_ShouldReturnPeopleList() throws JsonProcessingException {
+
+        var newContent = given()
+                .spec(specification)
+                .contentType(TestConfig.CONTENT_TYPE_JSON).body(person1)
+                .when().post()
+                .then().statusCode(200);
+
+        var content = given()
+                .spec(specification)
+                .when()
+                .get()
+                .then()
+                .statusCode(200)
+                .extract()
+                .body().asString();
+
+        // converter o objeto para um vetor
+        List<Person> list = List.of(objectMapper.readValue(content, Person[].class));
+
+        Person foundPersonOne = list.get(0);
+
+        assertNotNull(foundPersonOne);
+        assertNotNull(foundPersonOne.getId());
+        assertNotNull(foundPersonOne.getFirstName());
+        assertNotNull(foundPersonOne.getLastName());
+        assertNotNull(foundPersonOne.getAddress());
+        assertNotNull(foundPersonOne.getGender());
+        assertNotNull(foundPersonOne.getEmail());
+
+        assertTrue(foundPersonOne.getId() > 0);
+        assertEquals("Betina Isabella", foundPersonOne.getFirstName());
+        assertEquals("Farias", foundPersonOne.getLastName());
+        assertEquals("Avenida André Antônio Maggi, 636", foundPersonOne.getAddress());
+        assertEquals("Feminino", foundPersonOne.getGender());
+        assertEquals("betina.isabella.farias@tecnew.net", foundPersonOne.getEmail());
+
+        Person foundPersonTwo = list.get(1);
+
+        assertNotNull(foundPersonTwo);
+        assertNotNull(foundPersonTwo.getId());
+        assertNotNull(foundPersonTwo.getFirstName());
+        assertNotNull(foundPersonTwo.getLastName());
+        assertNotNull(foundPersonTwo.getAddress());
+        assertNotNull(foundPersonTwo.getGender());
+        assertNotNull(foundPersonTwo.getEmail());
+
+        assertTrue(foundPersonTwo.getId() > 0);
+        assertEquals("Tereza", foundPersonTwo.getFirstName());
+        assertEquals("Assis", foundPersonTwo.getLastName());
+        assertEquals("Alameda Venezuela, 937", foundPersonTwo.getAddress());
+        assertEquals("Feminino", foundPersonTwo.getGender());
+        assertEquals("terezacarolinaassis@imaxbrasil.com.br", foundPersonTwo.getEmail());
     }
 }
